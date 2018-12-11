ffd57ebc9a5187b5d9db3f473ec5cc577aa8d763
olso <olsansky.martin@gmail.com>
2018-12-11 20:23:32 +0100

    open

---
+++ index.js
@@ -0,0 +1,21 @@
+const opn = require('opn')
+const express = require('express')
+const path = require('path')
+
+const app = new express()
+const port = 80
+const publicDir = path.join(__dirname, 'public')
+
+app.use(express.static(publicDir))
+app.use(express.json())
+
+app.post('/open-url', (req, res) => {
+    const { body } = req
+    const { url } = body
+    
+    opn(url)
+
+    res.sendStatus(200)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
